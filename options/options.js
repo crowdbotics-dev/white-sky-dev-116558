b1eba6b45b0a0ed079034d50ee57e5a665f3ffdd
Crowdbotics <engineering@crowdbotics.com>
2023-09-14 23:23:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-sky-dev-116558",
+  url: "https://white-sky-dev-116558.botics.co",
+  api: "https://white-sky-dev-116558.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
